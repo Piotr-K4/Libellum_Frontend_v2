7718796c2ca67c2099ba65447ce5261f7c43f15b
Pietrek-K <pkusnierz95@protonmail.com>
2024-05-07 17:57:45 +0200

    Poprawiono menu-mobile, oraz jego wyświetlanie. Tak samo poprawiono input-serach.

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 function toggleMenu(){
     let mobileMenu = document.querySelector(".mobile-menu")
-
-
     mobileMenu.classList.toggle("mobile-menu--is-active")
-
 }
 
 function toggleSearchInput(){
@@ -11,8 +8,46 @@ function toggleSearchInput(){
     searchBox.classList.toggle("wrapper-search--mobile-is-active")
 }
 
-function toggleMenuLinks(option){
-    option.parentElement.nextElementSibling.classList.toggle("mobile-menu__links-is-active")
+// function toggleMenuLinks(option){
+//     option.firstElementChild.classList.toggle("mobile-menu__option--button-is-active")
+//     option.parentElement.nextElementSibling.classList.toggle("mobile-menu__links-is-active")
+// }
+
+function toggleMenuLinks(option) {
+    const arrowIcon = option.firstElementChild;
+    const menuLinks = option.parentElement.nextElementSibling;
+    console.log(window.innerWidth)
+
+    arrowIcon.classList.toggle("mobile-menu__option--button-is-active");
+
+    if (arrowIcon.classList.contains("mobile-menu__option--button-is-active")) {
+        arrowIcon.style.transition = "transform 0.2s ease-in-out";
+        arrowIcon.style.transform = "rotate(90deg)";
+        setTimeout(() => {
+            menuLinks.classList.toggle("mobile-menu__links-is-active");
+        }, 30);
+    } else {
+        arrowIcon.style.transition = "transform 0.2s ease-in-out";
+        arrowIcon.style.transform = "rotate(0deg)";
+        setTimeout(() => {
+            menuLinks.classList.toggle("mobile-menu__links-is-active");
+        }, 30);
+    }
 }
 
+
+window.addEventListener("resize", () => {
+    if ( document.querySelector(".mobile-menu").className === "mobile-menu mobile-menu--is-active"){
+        if(window.innerWidth >= 992){
+            toggleMenu();
+        }
+    }
+
+    if ( document.querySelector(".wrapper-search--mobile").className === "wrapper-search--mobile wrapper-search--mobile-is-active"){
+        if(window.innerWidth >= 992){
+            toggleSearchInput();
+        }
+    }
+}
+)
 
